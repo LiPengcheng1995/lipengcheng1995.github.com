32d3ad5d7194201547190e8035cb73b280544b20
LiPengcheng <1795811628@qq.com>
2016-11-29 21:58:17 +0800

    Add files via upload

---
+++ demo10/js/player_js.js
@@ -0,0 +1,284 @@
+var music = document.getElementById('player');
+var playWhat = 0;
+//music.play();
+//申请及初始化音量调节的操作
+var musicSound = 1;
+var ifChangingSound = false;
+var soundProgressAll = document.getElementById("soundProgressAll");
+var soundProgressAllData = soundProgressAll.getClientRects()[0];
+
+//申请及初始化音乐进度调节的操作
+var musicProgress = 0;
+var ifChangingMusic = false;
+var musicProgressAll = document.getElementById("musicProgressAll");
+var musicProgressAllData = musicProgressAll.getClientRects()[0];
+
+//设置音量调整
+soundProgressAll.onmousedown = function(event) {
+	//console.log(soundProgressAllLeft);
+	document.getElementById("soundProgressNow").style.width = (event.clientX - soundProgressAllData.left) / (soundProgressAllData.right - soundProgressAllData.left) * 100 + "%";
+	console.log((event.clientX - soundProgressAllData.left) / (soundProgressAllData.right - soundProgressAllData.left))
+	musicSound = (event.clientX - soundProgressAllData.left) / (soundProgressAllData.right - soundProgressAllData.left);
+	music.volume = musicSound;
+	ifChangingSound = true;
+}
+
+soundProgressAll.onmouseup = function() {
+	if(ifChangingSound == true) {
+		ifChangingSound = false;
+	}
+
+}
+
+document.getElementById("soundProgress").onmousemove = function(event) {
+	if(ifChangingSound) {
+		//console.log(soundProgressAllLeft);
+		document.getElementById("soundProgressNow").style.width = (event.clientX - soundProgressAllData.left) / (soundProgressAllData.right - soundProgressAllData.left) * 100 + "%";
+		//console.log((event.clientX - soundProgressAllData.left) / (soundProgressAllData.right - soundProgressAllData.left) + "%")
+		musicSound = (event.clientX - soundProgressAllData.left) / (soundProgressAllData.right - soundProgressAllData.left);
+		music.volume = musicSound;
+		ifChangingSound = true;
+	}
+}
+
+document.getElementById("soundProgress").onmouseup = function() {
+	if(ifChangingSound == true) {
+		ifChangingSound = false;
+	}
+}
+
+//设置音乐进度调整
+
+musicProgressAll.onmousedown = function(event) {
+	//console.log(soundProgressAllLeft);
+	document.getElementById("musicProgressNow").style.width = (event.clientX - musicProgressAllData.left) / (musicProgressAllData.right - musicProgressAllData.left) * 100 + "%";
+	//console.log(document.getElementById("musicProgressNow").style.width )
+	//	console.log((event.clientX - musicProgressAllData.left) / (musicProgressAllData.right - musicProgressAllData.left) * 100 + "%");
+	musicProgress = (event.clientX - musicProgressAllData.left) / (musicProgressAllData.right - musicProgressAllData.left);
+	ifChangingMusic = true;
+}
+
+musicProgressAll.onmouseup = function() {
+	if(ifChangingMusic == true) {
+		ifChangingMusic = false;
+		music.currentTime = parseInt(music.duration * musicProgress);
+	}
+
+}
+
+document.getElementById("progress").onmousemove = function(event) {
+	if(ifChangingMusic) {
+		//console.log(soundProgressAllLeft);
+		document.getElementById("musicProgressNow").style.width = (event.clientX - musicProgressAllData.left) / (musicProgressAllData.right - musicProgressAllData.left) * 100 + "%";
+		//console.log((event.clientX - soundProgressAllData.left) / (soundProgressAllData.right - soundProgressAllData.left) + "%")
+		musicProgress = (event.clientX - musicProgressAllData.left) / (musicProgressAllData.right - musicProgressAllData.left);
+		ifChangingMusic = true;
+	}
+}
+
+document.getElementById("progress").onmouseup = function() {
+	if(ifChangingMusic == true) {
+		ifChangingMusic = false;
+		music.currentTime = parseInt(music.duration * musicProgress);
+	}
+
+}
+
+//基本的ui界面完成了，接下来进行音乐播放的事件的绑定
+//设置music的时间监听
+//music.currentTime = '20s';
+//music.play();
+
+//设置时间的转换函数，直接写入timeNow和timeAll
+var arrayOfPassedWord;
+var Index = 0;
+
+function writeIntoTimeText(id, seconds) {
+	var min, sec;
+	if((seconds / 60) < 10) {
+		min = "0" + parseInt(seconds / 60);
+	} else {
+		min = seconds / 60;
+	}
+
+	if((seconds % 60) < 10) {
+		sec = "0" + (seconds % 60);
+	} else {
+		sec = seconds % 60;
+	}
+	//	console.log("min= "+min+" sec= "+sec);
+	document.getElementById(id).innerHTML = min + ":" + sec;
+	//接下来进行歌词的滚动
+	if(id == "timeNowText") {
+		var name = "time" + seconds;
+		if(document.getElementById(name) != null) {
+			//	console.log(seconds);		
+			//	console.log(name);
+			for(var i = 0; i < arrayOfPassedWord.length; i++) {
+				document.getElementById(arrayOfPassedWord[i]).style.color = "gray";
+				if(arrayOfPassedWord[i] == name) {
+					Index = i;
+				}
+			}
+			for(var i = 0; i < Index - 5; i++) {
+				document.getElementById(arrayOfPassedWord[i]).style.display = "none";
+			}
+			for(var i = Index - 5; i < arrayOfPassedWord.length; i++) {
+				if(i >= 0) {
+					document.getElementById(arrayOfPassedWord[i]).style.display = "block";
+				}
+			}
+			document.getElementById(name).style.color = "blue";
+			//	console.log(name);
+			//	console.log(name + "   " + Index + "   " + arrayOfPassedWord[Index]);
+
+		}
+		document.getElementById("cover").style.webkitTransform = "rotate(" + (seconds * 3) + "deg)";
+		//	console.log(seconds);
+		//	console.log("rotate("+(seconds*10)+"deg)");
+	}
+}
+
+//var last = 0;
+music.addEventListener("timeupdate", function() {
+	var currentTime = parseInt(this.currentTime);
+	//console.log(this.currentTime - last);
+	//last = this.currentTime;
+	writeIntoTimeText("timeNowText", currentTime);
+	if(music.readyState == 4) {
+		writeIntoTimeText("timeAllText", parseInt(music.duration));
+		musicProgress = currentTime / music.duration;
+		document.getElementById("musicProgressNow").style.width = musicProgress * 100 + "%";
+	}
+
+})
+
+document.getElementById("pouse").onclick = function() {
+	if(music.paused) {
+		music.play();
+		document.getElementById("pouse").style.backgroundPositionX = '-98px';
+	} else {
+		music.pause();
+		document.getElementById("pouse").style.backgroundPositionX = '-3px';
+	}
+}
+
+document.getElementById("soundIcon").onclick = function() {
+	if(music.muted) {
+		//现在是静音状态
+		music.muted = false;
+		document.getElementById("soundIcon").style.backgroundPositionX = '-70px';
+
+	} else {
+		//现在不是静音状态
+		music.muted = true;
+		document.getElementById("soundIcon").style.backgroundPositionX = '-105.5px';
+
+	}
+}
+
+//接下来进行歌词的加载及同步滚动
+//由于本地文件的访问有很大的限制，所以用一个大大大大大大大变量来存放吧
+
+loadAll();
+
+function loadAll() {
+	switch(playWhat) {
+		case 0:
+			music.pause();
+			music.src = "music/徐良 - 那时雨.mp3";
+			document.getElementById("cover").style.backgroundImage = "URL('music/徐良 - 那时雨.jpg')";
+			music.play();
+			document.getElementById("pouse").style.backgroundPositionX = '-98px';
+			break;
+		case 1:
+			music.pause();
+			music.src = "music/Adele - Rolling In The Deep.mp3";
+			document.getElementById("cover").style.backgroundImage = "URL('music/Adele - Rolling In The Deep.jpg')";
+			music.play();
+			document.getElementById("pouse").style.backgroundPositionX = '-98px';
+			break;
+		case 2:
+			music.pause();
+			music.src = "music/DJ OKAWARI - Flower Dance.mp3";
+			document.getElementById("cover").style.backgroundImage = "URL('music/DJ OKAWARI - Flower Dance.jpg')";
+			music.play();
+			document.getElementById("pouse").style.backgroundPositionX = '-98px';
+			break;
+		case 3:
+			music.pause();
+			music.src = "music/Eminem,Rihanna - Love the Way You Lie [Clean].mp3";
+			document.getElementById("cover").style.backgroundImage = "URL('music/Eminem,Rihanna - Love the Way You Lie [Clean].jpg')";
+			music.play();
+			document.getElementById("pouse").style.backgroundPositionX = '-98px';
+			break;
+		case 4:
+			music.pause();
+			music.src = "music/Groove Coverage - She.mp3";
+			document.getElementById("cover").style.backgroundImage = "URL('music/Groove Coverage - She.jpg')";
+			music.play();
+			document.getElementById("pouse").style.backgroundPositionX = '-98px';
+			break;
+		default:
+			break;
+	}
+	if (words[playWhat] == "本歌曲尚无歌词") {
+		document.getElementById("right").innerHTML = "<p>本歌曲尚无歌词</p>";
+		return;
+	}
+	document.getElementById("right").innerHTML = null;
+	arrayOfPassedWord = new Array();
+	var word = words[playWhat].split('[');
+	var line;
+	
+	for(var i = 1; i < word.length; i++) {
+		//	console.log(word[i]);
+		line = word[i].split(']');
+		//	console.log(line.length);
+		var lineTime = line[0].split(':');
+		var p = document.createElement("p");
+		p.id = "time" + (parseInt(lineTime[0] * 60 + parseInt(lineTime[1])));
+		p.innerHTML = line[1];
+		if(i == 1) {
+			document.getElementById("playedName").innerHTML = line[1];
+		} else if(i == 2) {
+			document.getElementById("playedPlayerName").innerHTML = line[1];
+		}
+		document.getElementById("right").appendChild(p);
+		arrayOfPassedWord.push("time" + (parseInt(lineTime[0] * 60 + parseInt(lineTime[1]))));
+	}
+		
+	
+
+}
+
+document.getElementById("lastOne").onclick = function() {
+	playWhat = (playWhat - 1 + 5) % 5;
+	loadAll();
+}
+document.getElementById("nextOne").onclick = function() {
+	playWhat = (playWhat + 1 + 5) % 5;
+	loadAll();
+}
+var aa = document.getElementsByClassName("item");
+console.log(aa[0])
+aa[0].onclick = function(){
+	playWhat = 0;
+	loadAll();
+}
+aa[1].onclick = function(){
+	playWhat = 1;
+	loadAll();
+}
+aa[2].onclick = function(){
+	playWhat = 2;
+	loadAll();
+}
+aa[3].onclick = function(){
+	playWhat = 3;
+	loadAll();
+}
+aa[4].onclick = function(){
+	playWhat = 4;
+	loadAll();
+}
---
+++ demo9/js/coverflow.js
@@ -0,0 +1,108 @@
+//过去当前容器的dom对象
+var containerObj = document.querySelector("#container");
+//获取所有的卡片
+var covers = document.querySelectorAll(".cover");
+
+//得到卡片长度
+var coverLength = covers.length;
+//中间卡片下标位置
+var middleIndex = (coverLength - 1) / 2;
+//中间卡牌你的dom对象
+var middleCover = covers[middleIndex];
+//设置当前的卡片，默认中间的
+var currentIndex = middleIndex;
+var containerWidth = containerObj.clientWidth;
+//3Dy轴最大旋转角度
+var maxRotate = 42;
+var maxZIndex = middleIndex + 1;
+//计算得出每一个卡片的累加旋转角度
+var stepper = maxRotate / middleIndex;
+//便利所有的卡片
+
+for(var i = 0; i < coverLength; i++) {
+	var cover = covers[i];
+	//默认所有水平平移0
+	cover.style.transform = "translateX(0px) rotateY(" + (maxRotate - stepper * i) + "deg)";
+	//设置zIndex
+	if(i < currentIndex) {
+		cover.style.zIndex = i + 1;
+
+	} else if(i === currentIndex) {
+		//当前的卡片显示更加宽
+		cover.style.flexGrow = 2;
+		cover.style.zIndex = i + 1;
+	} else {
+		cover.style.zIndex = maxZIndex - i;
+	}
+}
+//监听键盘事件响应
+
+
+var translateReg = /translateX\((\-?\d+\.?\d*)px\)/;
+var rotateReg = /rotateY\((\-?\d+\.?\d*)deg\)/;
+
+var currentTranslate, currentRotate;
+
+//设置随时间轮播部分，此部分为新加
+var t = setTimeout(time,2000);
+function time(){
+	clearTimeout(t);//清除定时器
+	dt = new Date(t);
+	var distination = (currentIndex + 1)%7;
+	moves(distination);
+	t = setTimeout(time,2000);
+}
+
+function moves(distination){
+	var times = currentIndex - distination;
+	if(times < 0){
+		//中心位置右移
+		move("left");
+	}
+	else{
+		//右边到头了，应该迅速回来
+		for (var i=0;i<6;i++) {
+			move("right");
+		}
+	}
+}
+//移动卡片
+function move(direction) {
+	//判断方向，更新当前卡片下标
+	direction == "right"?currentIndex--:currentIndex++;
+	//遍历数组
+	for(var i = 0; i < coverLength; i++) {
+		var cover = covers[i];
+		//为了获得卡片改变前的位移偏移值和旋转值
+		var prevTransate = parseInt(cover.style.transform.match(translateReg)[1]);
+		var prevRotate = parseInt(cover.style.transform.match(rotateReg)[1]);
+		//console.log(prevRotate);
+
+		//更具左右分别更改那个旋转量和偏移量
+		if(direction === "right") {
+			currentTranslate = prevTransate + (containerWidth / (coverLength + 1));
+			currentRotate = prevRotate - stepper;
+			//console.log(currentTranslate);
+		} else {
+			currentTranslate = prevTransate - (containerWidth / (coverLength + 1));
+			currentRotate = prevRotate + stepper;
+
+		}
+		cover.style.transform = "translateX(" + currentTranslate + "px) rotateY(" + currentRotate + "deg)";
+		if(i === currentIndex) {
+			cover.style.flexGrow = 2;
+			cover.style.zIndex = maxZIndex;
+
+		} else {
+			cover.style.flexGrow = 1;
+			if(i < currentIndex) {
+				cover.style.zIndex = i;
+			} else {
+				cover.style.zIndex = maxZIndex - i;
+			}
+
+		}
+
+	}
+}
+//console.log(middleIndex);
