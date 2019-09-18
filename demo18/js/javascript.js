19d20b633c5e7ba5d533dded467e2cecb39695f4
lipengcheng3 <lipengcheng3@jd.com>
2019-09-18 14:26:58 +0800

    修改

--- demo18/js/javascript.js
+++
@@ -1,7 +0,0 @@
-document.getElementsByClassName("show")[0].onclick = function(){
-	document.getElementsByClassName("nav-item")[0].style.display = 'block'
-	console.log(document.getElementsByClassName("nav-item")[0].style)
-	if(document.getElementsByClassName("nav-item")[0].style.display == ""){
-		alert("2")
-	}
-}
